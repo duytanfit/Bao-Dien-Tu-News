0801f277152332800f399cc5268fdad4f0d1602c
Hoai Thi <mailthi216@gmail.com>
2019-05-05 17:24:09 +0700

    update logined

--- js/active.js
+++ js/active.js
@@ -101,7 +101,7 @@
     // :: 6.0 ScrollUp Active Code
     if ($.fn.scrollUp) {
         browserWindow.scrollUp({
-            scrollSpeed: 1500,
+            scrollSpeed: 1800,
             scrollText: '<i class="fa fa-angle-up"></i>'
         });
     }
@@ -146,5 +146,36 @@
     navbar_toggler.on('click', function () {
         searchbtn.removeClass('fa-close');
     });
+    
 
 })(jQuery);
+$(document).ready(function()
+{
+        $(".account").click(function()
+            {
+            var X=$(this).attr('id');
+            if(X==1)
+            {
+                $(".submenu").hide();
+                $(this).attr('id', '0'); 
+                }
+            else
+                {
+                    $(".submenu").show();
+                    $(this).attr('id', '1');
+                }
+            });
+        $(".submenu").mouseup(function()
+            {
+                return false
+            });
+        $(".account").mouseup(function()
+            {
+        return false
+        });
+        $(document).mouseup(function()
+            {
+                $(".submenu").hide();
+                $(".account").attr('id', '');
+            });
+            });
